62a48bc16a6084f9df0083789f60ea253074b697
poshengchiang <posheng.chiang@apify.com>
2025-11-28 18:20:49 +0800

    feat: import constant file to use the start label

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ import { PlaywrightCrawler } from 'crawlee';
 // this is ESM project, and as such, it requires you to specify extensions in your relative imports
 // read more about this here: https://nodejs.org/docs/latest-v18.x/api/esm.html#mandatory-file-extensions
 // note that we need to use `.js` even when inside TS files
+import { LABELS } from './constants.js';
 import { router } from './routes.js';
 
 interface Input {
